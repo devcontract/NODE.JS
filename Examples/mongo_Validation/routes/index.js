3d18d5029e71a3a4e13428ec4cc6533d360536b7
kosankuz <konstantinkuzmenko@icloud.com>
2019-04-28 21:03:41 +0300

    db validation example

--- Examples/mongo_Validation/routes/index.js
+++ Examples/mongo_Validation/routes/index.js
@@ -17,6 +17,54 @@ router.get('/index2', function(req, res) {
     res.render('index2' );
 });
 
+router.post('/index', function (req, res, next) {
+
+// create a new user
+    var newUser = User({
+        email : req.body.email ,
+        salt : 19,
+        password : req.body.password,
+        firstname :req.body.firstname ,
+        lastname :req.body.lastname
+    });
+
+// save the user
+    newUser.save(function(err) {
+        if (err) {
+            //console.log(err);
+
+
+            req.flash('error', err);
+
+            //   arrayError = req.flash('error');
+
+            // return console.log(err);
+
+            //  var errorString = Buffer.from(err).toString();
+            //   errorString.replace('ValidationError: ','');
+            // console.log(errorString);
+            /* arrayError = arrayError.toString();
+             arrayError =  arrayError.replace('ValidationError: ','');
+             arrayError =  arrayError.replace(',','');
+             arrayError =  arrayError.replace('.,','');
+             arrayError =  arrayError.replace('required.,','required');
+             arrayError = arrayError.trim();
+             arrayError = arrayError.split(',');*/
+
+            return res.render('index', {error: req.flash('error')});
+        }
+
+        console.log('User created!');
+
+        req.flash('error','User Created');
+
+
+        res.render('index',{error:req.flash('error')});
+    });
+    // res.render('index', {error:req.flash('error')});
+
+});
+
 router.post('/index2', function (req, res, next) {
 
 // create a new user
